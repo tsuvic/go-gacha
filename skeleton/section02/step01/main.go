db85258f4df2c7c4b5542e0cb7fbe417292d77ba
tsuvi <admja11@gmail.com>
2022-10-17 12:55:05 +0900

    test

--- skeleton/section02/step01/main.go
+++ skeleton/section02/step01/main.go
@@ -9,15 +9,15 @@ import (
 )
 
 func main() {
-	
 
 	// 乱数の種を設定する
 	// 現在時刻をUNIX時間にしたものを種とする
 	rand.Seed(time.Now().Unix())
 
 	// TODO: 長さ11の文字列型の配列を変数resultとして定義する
+	var result [11]string
 
-	for i := 0; /* TODO: 継続条件をiがresultの要素数より小さい場合にする */; i++ {
+	for i := 0; i < len(result); i++ {
 
 		// 0から99までの間で乱数を生成する
 		num := rand.Intn(100)
