1f625b2240ec225d60b70949faa6a0244ffff43f
tsuvi <admja11@gmail.com>
2022-10-17 17:32:11 +0900

    a

--- skeleton/section04/step01/player.go
+++ skeleton/section04/step01/player.go
@@ -1,3 +1,29 @@
 package main
 
-// TODO: playerに関する処理をここに移す
+import (
+	"fmt"
+)
+
+type player struct {
+	tickets int
+	coin    int
+}
+
+func (p *player) drawableNum() int {
+	return p.tickets + p.coin/10
+}
+
+func (p *player) draw(n int) {
+	if p.drawableNum() < n {
+		fmt.Println("ガチャ券またはコインが不足しています")
+		return
+	}
+
+	if p.tickets > n {
+		p.tickets -= n
+		return
+	}
+
+	p.tickets = 0
+	p.coin -= n * 10 // 1回あたり10枚消費する
+}
